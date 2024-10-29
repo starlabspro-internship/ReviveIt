4e543b345baf3bdfd2c68d76a58d5f89b584d01b
betimtasholli <betim.tasholli1@student.uni-pr.edu>
2024-10-29 09:17:37 +0100

    feat/Added template to the project

---
+++ ReviveIT/WebUI/Controllers/LogInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ ReviveIT/WebUI/Controllers/RrethNeshController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class RrethNeshController : Controller
+    {
+        public IActionResult RrethNesh()
+        {
+            ViewBag.IsPunetPage = true;
+            return View();
+        }
+
+    }
+}
