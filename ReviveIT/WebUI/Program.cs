0179c64856bc3718ff6568c20794c101604660d5
betimtasholli <betim.tasholli1@student.uni-pr.edu>
2024-10-29 14:05:05 +0100

    feat/Added ApplicationDbContext Class

--- ReviveIT/WebUI/Program.cs
+++ ReviveIT/WebUI/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    b=>b.MigrationsAssembly("WebUI")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +15,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
