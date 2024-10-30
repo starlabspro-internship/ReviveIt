0aa1256260f2c7741b22e75bf85df153962954d2
betimtasholli <betim.tasholli1@student.uni-pr.edu>
2024-10-30 11:36:03 +0100

    feat/Added SubscriptionTable

---
+++ ReviveIT/Domain/Entities/Subscriptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Subscriptions
+    {
+        [Key]
+        public int SubscriptionId { get; set; } // Primary key
+        public string PlanType { get; set; }
+        public string Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Foreign key to User
+        public int UserId { get; set; }
+        // public User User { get; set; }
+    }
+}
