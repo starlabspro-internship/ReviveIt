b4c950e22d975cf84f585afdc21d29847f85eea8
redonademi <redonademi@hotmail.com>
2024-10-31 15:27:36 +0100

    Kam shtuar Messages entityConfiguration and updated applicationdbcontext

---
+++ ReviveIT/Domain/Configurations/MessagesConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Entities;
+
+namespace Domain.Configurations
+{
+    public class MessagesConfigurations : IEntityTypeConfiguration<Messages>
+    {
+        public void Configure(EntityTypeBuilder<Messages> builder)
+        {
+            builder.HasKey(m => m.MessageID);
+
+            builder.Property(m => m.MessageContent)
+                   .IsRequired()
+                   .HasMaxLength(500);
+
+            builder.Property(m => m.Timestamp)
+                   .HasDefaultValueSql("GETDATE()");
+
+            builder.HasOne(m => m.Sender)
+                   .WithMany()
+                   .HasForeignKey(m => m.SenderID)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(m => m.Recipient)
+                   .WithMany()
+                   .HasForeignKey(m => m.RecipientID)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
