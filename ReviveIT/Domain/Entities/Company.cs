80ba915b6dc98a53e36da2dfc46746b5d64bb251
dkacoli <159468119+dkacoli@users.noreply.github.com>
2024-11-06 10:07:52 +0100

    Added Company Table

--- ReviveIT/Domain/Entities/Company.cs
+++ ReviveIT/Domain/Entities/Company.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace Domain.Entities
 {
     public class Company
     {
-        [Key]
-
+    
         public Guid Id { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
