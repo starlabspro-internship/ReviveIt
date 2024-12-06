7a95ce819af6682d3d2c6748ef86974d94ba45c7
redonademi <redonademi@hotmail.com>
2024-12-06 15:51:48 +0100

    Added message_service and imessage_service to handle chat operations

---
+++ ReviveIT/Application/Interfaces/IMessageService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Application.Interfaces
+{
+    public interface IMessageService
+    {
+        void SendMessage(string senderId, string receiverId, string message);
+        IEnumerable<ChatSession> GetChatSessions(string userId);
+        IEnumerable<Messages> GetChatHistory(int chatSessionId);
+        void MarkMessageAsRead(int messageId);
+    }
+}
