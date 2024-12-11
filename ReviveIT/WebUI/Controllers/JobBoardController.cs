98078287c66f5eea619deea926f75d62b4324cd1
Nathan Amanuel <nathana931@gmail.com>
2024-12-11 13:25:06 +0100

    job board with accessory changes on profile page

---
+++ ReviveIT/WebUI/Controllers/JobBoardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    [Route("JobBoard")]
+    public class JobBoardController : Controller
+    {
+        [HttpGet("JobBoard")] // Maps to: https://localhost:7018/JobBoard/JobBoard
+        public IActionResult JobBoard()
+        {
+            return View(); // Looks for a corresponding View file in Views/JobBoard/JobBoard.cshtml
+        }
+    }
+}
