68280e5f4ed780543aef5b22e97c1450283ce07b
redonademi <redonademi@hotmail.com>
2024-12-18 08:44:17 +0100

    Add Settings view with sidebar, main content, and pop-up forms

---
+++ ReviveIT/WebUI/Controllers/SettingsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Domain.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace WebUI.Controllers
+{
+    [Authorize]
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Settings");
+        }
+    }
+}
