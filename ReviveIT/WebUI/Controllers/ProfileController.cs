2e2576050a072156893d0603f59efbffd7134b5a
Betim Tasholli <58818151+betimtasholli@users.noreply.github.com>
2024-12-19 16:11:06 +0100

    feat/Implemented edit categories feature in UserProfile

--- ReviveIT/WebUI/Controllers/ProfileController.cs
+++ ReviveIT/WebUI/Controllers/ProfileController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Application.DTO;
+using Microsoft.EntityFrameworkCore;
+using Application.Interfaces;
 
 namespace WebUI.Controllers
 {
@@ -12,9 +14,12 @@ namespace WebUI.Controllers
     public class ProfileController : Controller
     {
         private readonly UserManager<Users> _userManager;
-        public ProfileController(UserManager<Users> userManager)
+        private readonly IApplicationDbContext _context;
+
+        public ProfileController(UserManager<Users> userManager, IApplicationDbContext context)
         {
             _userManager = userManager;
+            _context = context;
         }
 
         public IActionResult Profile()
@@ -81,5 +86,50 @@ namespace WebUI.Controllers
             return Ok(new { message = "Description updated successfully." });
         }
 
+        [HttpGet("get-selected-categories")]
+        public async Task<IActionResult> GetSelectedCategories()
+        {
+            var userId = User.FindFirst("UserId")?.Value;
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized(new { message = "User not authenticated" });
+
+            var userCategories = await _context.UserCategories
+                .Where(uc => uc.UserId == userId)
+                .Select(uc => new
+                {
+                    uc.Category.CategoryID,
+                    uc.Category.Name
+                })
+                .ToListAsync();
+
+            return Ok(userCategories);
+        }
+
+        [HttpPost("updateCategories")]
+        public async Task<IActionResult> UpdateCategories([FromBody] List<int> categoryIds)
+        {
+            if (categoryIds == null || !categoryIds.Any())
+            {
+                return BadRequest(new { message = "Category list cannot be empty." });
+            }
+
+            var userId = User.FindFirst("UserId")?.Value;
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var existingUserCategories = _context.UserCategories.Where(uc => uc.UserId == userId);
+            _context.UserCategories.RemoveRange(existingUserCategories);
+
+            var newUserCategories = categoryIds.Select(categoryId => new UserCategory
+            {
+                UserId = userId,
+                CategoryId = categoryId
+            });
+
+            await _context.UserCategories.AddRangeAsync(newUserCategories);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Categories updated successfully" });
+        }
     }
 }
