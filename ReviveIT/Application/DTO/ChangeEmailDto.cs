4d94a26168490d0e4b80c4399b871d4f6d9cdf70
redonademi <redonademi@hotmail.com>
2024-12-24 18:57:50 +0100

    Added change email address feature

---
+++ ReviveIT/Application/DTO/ChangeEmailDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO
+{
+    public class ChangeEmailDto
+    {
+        [Required]
+        [EmailAddress]
+        public string CurrentEmail { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string NewEmail { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+}
