1520b739db48f4fae8e80fff3e6bae3bffa5b2e4
Betim Tasholli <58818151+betimtasholli@users.noreply.github.com>
2024-12-24 09:14:50 +0100

    feat/Added experience, role, sort by and reset filters in Hire a Pro Page

--- ReviveIT/WebUI/wwwroot/js/pros.js
+++ ReviveIT/WebUI/wwwroot/js/pros.js
@@ -29,6 +29,9 @@ function getFilterParameters(useUrl = false) {
             keywords: document.getElementById('keywords').value || null,
             selectedCityId: document.getElementById('filterCities').value || null,
             selectedCategoryId: document.getElementById('filterCategories').value || null,
+            experience: document.getElementById('experience').value || null,
+            selectRole: document.getElementById('selectRole').value || null,
+            sortBy: document.getElementById('sortBy').value || null,
         };
     }
 }
@@ -39,12 +42,19 @@ async function loadTechnicians(clear = false, useUrl = false) {
     }
     const takeCount = skipCount === 0 ? initialPageSize : additionalPageSize;
     const { keywords, selectedCityId, selectedCategoryId } = getFilterParameters(useUrl);
+    const experience = document.getElementById('experience').value || null;
+    const selectRole = document.getElementById('selectRole').value || null;
+    const sortBy = document.getElementById('sortBy').value || null;
+
     const params = new URLSearchParams({
         skipCount,
         takeCount,
         ...(keywords && { keywords }),
         ...(selectedCityId && { selectedCityId }),
         ...(selectedCategoryId && { selectedCategoryId }),
+        ...(experience && { experience }),
+        ...(selectRole && { selectRole }),
+        ...(sortBy && { sortBy }),
     });
 
     const response = await fetch(`/Pros/api/GetPros?${params.toString()}`, {
@@ -191,12 +201,25 @@ function fetchCitites(selectedCityId = null) {
         });
 }
 
+document.getElementById('resetFiltersButton').addEventListener('click', function () {
+    document.getElementById('filtersForm').reset();
+
+    document.getElementById('filterCities').value = '';
+    document.getElementById('filterCategories').value = '';
+    document.getElementById('keywords').value = '';
+    document.getElementById('experience').value = '';
+    document.getElementById('selectRole').value = '';
+    document.getElementById('sortBy').value = '';
+});
+
 document.addEventListener('DOMContentLoaded', async () => {
     await checkAuthenticationStatus();
     const { keywords, selectedCityId, selectedCategoryId } = getFilterParameters(true);
     await Promise.all([fetchCategories(selectedCategoryId), fetchCitites(selectedCityId)]);
+
     if (keywords) document.getElementById('keywords').value = keywords;
     loadTechnicians(true, true);
+
     const applyFiltersButton = document.getElementById('applyFiltersButton');
 
     if (applyFiltersButton) {
@@ -206,7 +229,9 @@ document.addEventListener('DOMContentLoaded', async () => {
             loadTechnicians(true, false);
         });
     }
+
     const viewMoreButton = document.getElementById('viewMoreButton');
+
     if (viewMoreButton) {
         viewMoreButton.addEventListener('click', (e) => {
             e.preventDefault();
