89ba4deaab37a55e6f558b7ad700a4ef18069f1a
donatgosalcii <gosalci38@gmail.com>
2024-12-27 14:15:27 +0100

    Removed inbox from the dropdown and fixed an issue with custom.js when accessing inbox

--- ReviveIT/WebUI/wwwroot/js/custom.js
+++ ReviveIT/WebUI/wwwroot/js/custom.js
@@ -1,7 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    getYear();
+});
+
 function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+    var displayYearElement = document.querySelector("#displayYear");
+    if (displayYearElement) {
+        displayYearElement.innerHTML = currentYear;
+    } else {
+        console.error("Element with ID 'displayYear' not found.");
+    }
 }
-
-getYear();
